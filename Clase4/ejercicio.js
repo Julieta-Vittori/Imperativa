3b35fa3b2154a4f413756b8438fd4dd0477bcd43
Julieta-Vittori <julieta.vittori@gmail.com>
2021-08-17 21:37:52 -0300

    actividad clase 4

---
+++ Clase4/ejercicio.js
@@ -0,0 +1,25 @@
+//Actividad 1
+
+const horarioDeDespertar = 7.30;
+let tomoCafe = true;
+let lavoLosDientes = true;
+let horarioDeSalida = 8.30;
+let busACoger = "Ruta 4";
+console.log(tomoCafe, "\n"+ lavoLosDientes, "\n"+ horarioDeSalida, "\n"+ busACoger, "\n");
+
+//Actividad 2
+
+
+//Desafío extra
+
+let cajonPantalones = "Remeras";
+let cajonRemeras = "Pantalones";
+let cajonVacio;
+
+cajonVacio = cajonPantalones; //llevo remeras al cajón vacío
+cajonPantalones = cajonRemeras; // cambio pantalones al cajón de los pantalones
+cajonRemeras = cajonVacio //Paso remeras a cajón remeras
+
+console.log("cajonPantalones:",  cajonPantalones, "\n"+ "cajonRemeras:", cajonRemeras);
+
+
